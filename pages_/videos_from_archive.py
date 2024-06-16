c4f4a27e01c6e54bd76f5a7592df83de33f8a9b0
Stepan Ukolov <124688716+Y1OV@users.noreply.github.com>
2024-06-16 06:46:31 +0300

    Update videos_from_archive.py

--- pages_/videos_from_archive.py
+++ pages_/videos_from_archive.py
@@ -9,6 +9,9 @@ from PIL import Image, UnidentifiedImageError
 import io
 import os
 
+from funcc.risovashka import draw_bounding_box, visualize_annotations
+from funcc.ostalnoe import get_recommendations, process_zip_file
+
 # HTML-код для логотипа
 html_code = '''
 <div style="text-align: center;">
@@ -18,92 +21,8 @@ html_code = '''
 </div>
 '''
 
-# Функция для отрисовки рамок вокруг дефектов
-def draw_bounding_box(image, box, class_id, class_names, colors, box_thickness=3, font_scale=0.8, font_thickness=2):
-    height, width, _ = image.shape
-    x_center, y_center, w, h = box
 
-    xmin = int((x_center - w / 2) * width)
-    xmax = int((x_center + w / 2) * width)
-    ymin = int((y_center - h / 2) * height)
-    ymax = int((y_center + h / 2) * height)
 
-    color = colors[class_id]
-
-    cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, box_thickness)
-
-    label = f"{class_names[class_id]}"
-    (label_width, label_height), baseline = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, font_scale, font_thickness)
-
-    cv2.rectangle(image, (xmin, ymin - label_height - baseline), (xmin + label_width, ymin), color, cv2.FILLED)
-    cv2.putText(image, label, (xmin, ymin - baseline), cv2.FONT_HERSHEY_SIMPLEX, font_scale, (255, 255, 255), font_thickness)
-
-# Функция для визуализации аннотаций
-def visualize_annotations(image, annotations, class_names, colors, box_thickness=3, font_scale=0.8, font_thickness=2):
-    image = np.array(image)
-    unique_class_ids = set()
-    for annotation in annotations:
-        class_id = int(annotation['class_id'])
-        unique_class_ids.add(class_id)
-        box = (annotation['rel_x'], annotation['rel_y'], annotation['width'], annotation['height'])
-        draw_bounding_box(image, box, class_id, class_names, colors, box_thickness, font_scale, font_thickness)
-
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    unique_classes = [class_names[id] for id in unique_class_ids]
-    return image, unique_classes
-
-# Функция для получения рекомендаций по дефектам
-def get_recommendations(unique_classes):
-    recommendations_dict = {
-        'adj': "Используйте правильные параметры сварки. Поддерживайте чистоту сварочной поверхности. Проверьте качество сварочных материалов. Применяйте антипригарные спреи или пасты. Следите за стабильностью дуги и избегайте слишком длинной дуги.",
-        'int': "Завершайте сварку с заполнением кратера. Очистите поверхность от шлака после каждого прохода. Улучшите подготовку краев. Избегайте влажности в сварочных материалах. Уменьшите ток или скорость подачи проволоки. Поддерживайте чистоту сварочных материалов.",
-        'geo': "Уменьшите ток или скорость сварки. Улучшите подготовку краев. Увеличьте скорость сварки или уменьшите подачу материала. Проверьте параметры сварки для оптимального расплавления. Следите за балансом тепла и подачи материала. Проверьте настройки сварочного оборудования.",
-        'pro': "Применяйте правильные методы резки и шлифовки. Используйте подходящие инструменты для удаления заусенцев. Следите за аккуратностью резки и формовки. Убедитесь в правильном использовании инструментов и их остроте. Следите за осторожным обращением с материалами и готовыми изделиями.",
-        'non': "Улучшите подготовку краев и стыковку элементов. Применяйте методы заполнения и оптимальные параметры сварки. Используйте более качественные сварочные материалы. Проверьте настройки сварочного оборудования и параметры сварки. Убедитесь, что подготовка краев и очистка поверхности выполнены правильно."
-    }
-    recommendations = {}
-    for cls in unique_classes:
-        if cls in recommendations_dict:
-            recommendations[cls] = recommendations_dict[cls]
-    return recommendations
-
-# Функция для обработки изображений из ZIP-архива
-def process_zip_file(zip_file, class_names, colors):
-    results = []
-    with zipfile.ZipFile(zip_file, 'r') as archive:
-        for idx, file_info in enumerate(archive.infolist()):
-            file_name = file_info.filename
-            if file_name.lower().endswith(('.jpg', '.jpeg', '.png')):
-                try:
-                    # Очистка имени файла
-                    safe_name = os.path.basename(file_name)
-                    safe_name = safe_name.replace("(", "").replace(")", "").replace(" ", "_")
-
-                    with archive.open(file_info) as file:
-                        image = Image.open(file)
-                        image = image.convert("RGB")  # Конвертация изображения в RGB
-                        img_array = np.array(image)
-                        _, img_encoded = cv2.imencode('.jpg', img_array)
-                        img_bytes = img_encoded.tobytes()
-
-                        addr = 'https://6152-83-143-66-61.ngrok-free.app'
-                        test_url = addr + '/api/test'
-                        content_type = 'image/jpeg'
-                        headers = {'content-type': content_type}
-
-                        response = requests.post(test_url, data=img_bytes, headers=headers)
-
-                        if response.status_code == 200:
-                            annotations = json.loads(response.text)
-                            annotated_image, unique_classes = visualize_annotations(image, annotations, class_names, colors)
-                            results.append((f"{idx}_{safe_name}", annotated_image, unique_classes))
-                        else:
-                            st.error(f"Ошибка при получении аннотаций для файла {safe_name}.")
-                except UnidentifiedImageError:
-                    pass
-                except Exception as e:
-                    pass
-    return results
 
 # Основная функция Streamlit приложения
 def main():
