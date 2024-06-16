51721a060587bbb83a6aee45792c2198ad5f5756
Stepan Ukolov <124688716+Y1OV@users.noreply.github.com>
2024-06-16 05:45:45 +0300

    Update __init__.py

--- pages_/__init__.py
+++ pages_/__init__.py
@@ -7,7 +7,7 @@ from .main import main as main
 Page = namedtuple("Page", "title method")
 
 pages: dict[str, Page] = {
-    'Главная': Page(title="История распознаваний", method=main),
+    'Главная': Page(title="Главная", method=main),
     'archive': Page(title="Разпознать из архива", method=videos_from_archive),
 
 }
